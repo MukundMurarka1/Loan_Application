274b2591ee5bda62a5d9fd092ff67fc6f5895b3d
MukundMurarka1 <mk847225@gmail.com>
2023-01-30 18:01:43 +0530

    second

--- LoanApplication/src/Loan.java
+++ LoanApplication/src/Loan.java
@@ -1,8 +1,30 @@
+class LoanApplication{
+	static int rateOfInterstforPL=12;
+	static int rateOfInterstforEL=11;
+	static int rateOfInterstforHL=6;
+	static int rateOfInterstforcL=10;
+	
+}
+class PersonalLoan extends LoanApplication{
+	public void calculate_interest() {
+		
+	}
+	
+}
+class EducationLoan extends LoanApplication{
+	
+}
+class CarLoan extends LoanApplication{
+	
+}
+class HomeLoan extends LoanApplication{
+	
+}
 
 class Loan {
 
 	public static void main(String[] args) {
-		System.out.println("WELCOME TO OUR BANK ");
+		
 
 	}
 
