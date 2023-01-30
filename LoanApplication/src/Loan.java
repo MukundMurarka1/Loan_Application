d23c00d7bfaf59ac4fffb924e73af2d7f2cc1262
MukundMurarka1 <mk847225@gmail.com>
2023-01-30 18:10:24 +0530

    second

--- LoanApplication/src/Loan.java
+++ LoanApplication/src/Loan.java
@@ -6,18 +6,35 @@ class LoanApplication{
 	
 }
 class PersonalLoan extends LoanApplication{
-	public void calculate_interest() {
+	public void calculate_interest(int amt, int time ) {
+		int interest  =(amt*time*super.rateOfInterstforPL)/100;
+		System.out.println("The total amount you have to paid after completion of "+ time+ "is " +(amt+interest));
 		
 	}
 	
 }
 class EducationLoan extends LoanApplication{
+	public void calculate_interest(int amt, int time ) {
+		int interest  =(amt*time*super.rateOfInterstforEL)/100;
+		System.out.println("The total amount you have to paid after completion of "+ time+ "is " +(amt+interest));
+		
+	}
 	
 }
 class CarLoan extends LoanApplication{
+	public void calculate_interest(int amt, int time ) {
+		int interest  =(amt*time*super.rateOfInterstforcL)/100;
+		System.out.println("The total amount you have to paid after completion of "+ time+ "is " +(amt+interest));
+		
+	}
 	
 }
 class HomeLoan extends LoanApplication{
+	public void calculate_interest(int amt, int time ) {
+		int interest  =(amt*time*super.rateOfInterstforHL)/100;
+		System.out.println("The total amount you have to paid after completion of "+ time+ "is " +(amt+interest));
+		
+	}
 	
 }
 
